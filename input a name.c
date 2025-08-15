aa0c7a7d45ddc9d1a34d38be77f8d056203a45c5
MD. Rafiul Islam <rafiulislam316@gmail.com>
2025-08-15 14:51:30 +0600

    Create input a name.c
    inputting lines

---
+++ input a name.c
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    char Sellars_name[10];
+    printf("Enter Sellars name ");
+    scanf("%s",Sellars_name);
+    printf("Sellars name is %s.\n",Sellars_name);
+
+    return 0;
+}
