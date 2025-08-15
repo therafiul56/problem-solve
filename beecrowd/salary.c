0c38bd110b7bb01a4fdb103e6d7f061bb2b23099
MD. Rafiul Islam <rafiulislam316@gmail.com>
2025-08-15 10:09:02 +0600

    Create salary.c
    beecrowd | 1008
    Salary
    Adapted by Neilor Tonin, URI  Brazil
    
    Timelimit: 1
    Write a program that reads an employee's number, his/her worked hours number in a month and the amount he received per hour. Print the employee's number and salary that he/she will receive at end of the month, with two decimal places.
    
    Don’t forget to print the line's end after the result, otherwise you will receive “Presentation Error”.
    Don’t forget the space before and after the equal signal and after the U$.
    Input
    The input file contains 2 integer numbers and 1 value of floating point, representing the number, worked hours amount and the amount the employee receives per worked hour.
    
    Output
    Print the number and the employee's salary, according to the given example, with a blank space before and after the equal signal.
    
    Input Samples	
    Output Samples
    
    1.
    25
    100
    5.50
    
    NUMBER = 25
    SALARY = U$ 550.00
    2.
    1
    200
    20.50
    
    NUMBER = 1
    SALARY = U$ 4100.00
    3.
    6
    145
    15.55
    
    NUMBER = 6
    SALARY = U$ 2254.75

---
+++ beecrowd/salary.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+   int num, Work_h;
+   float Earn_h , SALARY ;
+   scanf("%d%d%f",&num,&Work_h,&Earn_h);
+   SALARY = Work_h*Earn_h;
+   printf("NUMBER = %d\nSALARY = U$ %.2f\n",num,SALARY);
+   
+ 
+    return 0;
+}
