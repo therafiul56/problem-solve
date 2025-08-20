7a78b5662e8acee1d0b29ba109ce4b96aa92ccd0
MD. Rafiul Islam <rafiulislam316@gmail.com>
2025-08-20 20:37:39 +0600

    Create factorials.c

---
+++ prob i cant do now/factorials.c
@@ -0,0 +1,24 @@
+   // Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int A,B,func=1;
+    scanf("%d",&A);
+      for(int j=1 ; j<=A; j++){
+                scanf("%d",&B);
+
+    for(int i=1; i<=B ; i++){
+        
+            func=func*i;
+
+return (int j=1) ;
+        
+    }
+    printf("%d\n",func);
+      }
+    
+
+    
+
+    return 0;
+}
