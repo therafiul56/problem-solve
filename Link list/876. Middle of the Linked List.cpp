b737f68c5148a7158d56d193bd113f15169283e1
MD. Rafiul Islam <rafiulislam316@gmail.com>
2026-01-09 13:14:09 +0600

    Add solution for finding middle of linked list
    Implement a program to find the middle of a linked list.

---
+++ Link list/876. Middle of the Linked List.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector <int> v;
+    while(true) {
+        char c;
+        cin >> c ;
+        if (c==']') break;
+        int a;
+        cin >> a;
+        v.push_back(a);
+    }
+	list <int> l;
+	for (auto a : v){
+	    l.push_back(a);
+	}
+	int s = l.size();
+	if(s%2==0) {
+	   int p=((s/2));
+	   cout << "[" << *next(l.begin(),p) ;
+	   for (int i=p+1 ; i<s ; i++) {
+	       cout << "," << *next(l.begin(),i) ;
+	   }
+	   cout << "]";
+	}
+	
+
+}
