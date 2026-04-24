3513a87df8cc8d8fb333c6ac979f545665d89584
MD. Rafiul Islam <rafiulislam316@gmail.com>
2026-04-25 00:16:41 +0600

    Refactor BankData and SecureBank classes

--- prob i cant do now/sectin 6.cpp
+++ prob i cant do now/sectin 6.cpp
@@ -1,12 +1,12 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 class BankData{
     public:
-    string mail,password,Loan_ammount,Income,Expense;
+    string mail,password;
+    int Loan_ammount,Income,Expense;
     
-    BankData(){
+    virtual void data_input(){
         cout << "Enter email : "; cin >> mail; cout << endl;
         cout << "enter password : "; cin >> password ; cout << endl;
         cout << "enter Loan_ammount : "; cin >> Loan_ammount ; cout << endl;
@@ -17,29 +17,106 @@ class BankData{
     
 };
 class SecureBank :virtual public BankData{
-    void chek_mail(string m){
-        try{
-            if(((m.find("@")!=string::npos) && (m.find(".com") != strong :: npos)) == false ){
-                throw invalid_argument("invalid email")
+    public:
+    
+    bool chekmail(string m){
+    if((m.find("@")!=string::npos) && (m.find(".com") != string :: npos)){
+                return false;
             }
-        }  catch(invalid_argument e){
-            cout << e.what() << endl; 
-            cout << "Enter mail : ";
-            cin << mail; chek_mail(mail);
+            else return true;
+}
+    void chek_mail(){
+        
+           while(true){
+                try{
+                if(chekmail(mail)){
+                    throw invalid_argument("invalid email");
+                }
+                cout << "valid email : " << mail << endl;break;
+    
+                }  catch(invalid_argument e){
+                    cout << e.what() << endl; 
+                    
+                }
+                cout << "Enter mail : ";
+                cin >> mail;
+           }
         }
-    }
+    
     
     void chek_pass(){
-        try{
-            if()
+        while(true){
+            try{
+            if(!FP(password)){
+                throw  invalid_argument("invalid password ! \n");
+            }
+            cout << "valid password : " << password << endl;break;
+        }catch(invalid_argument e){
+            cout << e.what() << endl;
+            cout << "Enter new password : " ; cin >> password ;
+        }
         }
     }
     bool FP(string s){
-        for(s:char c){
+    bool flag1 = false,flag2=false,flag3=false;
+        for(char c : s ){
             if(!isalnum(c) && !isspace(c)){
-                return true;
+                flag1 = true;
+                break;
             }
-          else return false;
+            // cout << "This is working!!";
+            // return false;
+        }
+        for (char c: s){
+        if(c > 'A' && c <'Z' ){
+            flag2 = true;
+            break;
+        }
+        // return false;
+    }
+    for(char  c : s){
+            if(c>'a' && c< 'z'){
+                flag3 = true;
+                break;
+            }
+            // return false;
+        }
+        return (flag1&&flag2&&flag3);
+        
+}
+void  chek_loan(){
+        while(true){
+            try{
+                if(Loan_ammount<0){
+                    throw invalid_argument("the loan is negetive");
+                }
+                cout << "valid loan number : "<< Loan_ammount<< endl;break;
+            }
+            
+            catch(const invalid_argument e){
+                cout << e.what() << endl << "enter a new loan numjber : " ;
+                cin >> Loan_ammount; 
+            }
+        }
+    }
+void chek_income(){
+        while(true){
+            try{
+                if(Income == Expense){
+                    throw invalid_argument("you need to earn more !!");
+                }
+                cout << "you earn enough to get loan " << endl
+                     << "THE risk is : " << (Loan_ammount/ (Income-Expense));
+                
+            break;
+            }
+            
+            catch (const invalid_argument e){
+                cout << e.what() << endl << "earn more ? ";
+                cin >> Income;
+                
+            }
+            
         }
     }
     
@@ -47,5 +124,11 @@ class SecureBank :virtual public BankData{
 
 
 int main() {
-
+    SecureBank s1;
+    s1.data_input();
+    s1.chek_mail();
+    s1.chek_pass();
+    s1.chek_loan();
+    s1.chek_income();
+    return 0;
 }
